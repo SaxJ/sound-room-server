6b0b288bc8853a0e67de1453a736644b42aaa541
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-25 15:32:16 +0800

    Finish websocket server

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Data.UUID (UUID)
-import qualified Network.WebSockets as WS
+import Control.Concurrent (modifyMVar_, newMVar, readMVar)
+import Control.Concurrent.MVar (MVar)
+import Control.Monad (forM_, forever, when)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Control.Monad (forM_, forever)
-import Control.Concurrent ( newMVar, readMVar, modifyMVar_ )
-import Control.Concurrent.MVar (MVar)
+import Data.UUID (UUID)
 import Data.UUID.V4 (nextRandom)
 import GHC.IO (finally)
+import qualified Network.WebSockets as WS
+import qualified Text.Printf as T
 
 type Client = (UUID, WS.Connection)
 
 type ServerState = [Client]
+
+validEvents :: [T.Text]
+validEvents = ["cheer", "clap", "cry", "laugh", "woof", "quack", "boo", "wolf", "drum", "lame"]
+
+isValidEvent :: T.Text -> Bool
+isValidEvent msg = any (== msg) validEvents
 
 newServerState :: ServerState
 newServerState = []
@@ -25,16 +33,21 @@ clientExists :: Client -> ServerState -> Bool
 clientExists client = any ((== fst client) . fst)
 
 addClient :: Client -> ServerState -> ServerState
-addClient c cs = if clientExists c cs then cs else c:cs
+addClient c cs = if clientExists c cs then cs else c : cs
 
 removeClient :: Client -> ServerState -> ServerState
 removeClient c = filter (\x -> fst x /= fst c)
 
+broadcastLog :: T.Text -> T.Text
+broadcastLog m = T.concat [action, m]
+  where
+    action = if isValidEvent m then "Broadcasting: " else "Skipping: "
+
 -- send message to all clients
 broadcast :: T.Text -> ServerState -> IO ()
 broadcast msg clients = do
-  T.putStrLn msg
-  forM_ clients (\(_, conn) -> WS.sendTextData conn msg)
+  T.putStrLn $ broadcastLog msg
+  forM_ clients (\(_, conn) -> when (isValidEvent msg) $ WS.sendTextData conn msg)
 
 main :: IO ()
 main = do
@@ -50,11 +63,11 @@ application state pending = do
     msg <- WS.receiveData conn
     case msg of
       _ | otherwise -> flip finally disconnect $ do
-                          modifyMVar_ state $ \s -> do
-                            let s' = addClient client s
-                            broadcast msg s'
-                            return s'
-                          talk client state
+        modifyMVar_ state $ \s -> do
+          let s' = addClient client s
+          broadcast msg s'
+          return s'
+        talk client state
         where
           client = (name, conn)
           disconnect = do
