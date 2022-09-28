793b898d80f7b57d2acd0c17e60428f541a98e4c
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-28 14:39:51 +0800

    NODE

---
+++ index.js
@@ -0,0 +1,24 @@
+import WebSocket, { WebSocketServer } from "ws";
+import validator from "validator";
+
+const wss = new WebSocketServer({ port: 9160 });
+wss.on("connection", (ws, req) => {
+  console.log(`Connection: ${req.url}`);
+
+  const roomName = req.url.replace("/", "");
+  if (!validator.isUUID(roomName)) {
+    ws.close(null, "Invalid room id");
+  }
+
+  ws.on(roomName, (data) => {
+    ws.send(data);
+  });
+
+  ws.on("message", (data, isBinary) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.emit(roomName, data);
+      }
+    });
+  });
+});
