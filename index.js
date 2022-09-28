7afe9be3eb0fd7fd068b8fde58f5c4ea926268f6
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-28 21:08:47 +0800

    Just close

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ import validator from "validator";
 
 const wss = new WebSocketServer({ port: 9160 });
 wss.on("connection", (ws, req) => {
-  console.log(`Connection: ${req.url}`);
+    console.log(`Connection: ${req.url}`);
 
-  const roomName = req.url.replace("/", "");
-  if (!validator.isUUID(roomName)) {
-    ws.close(null, "Invalid room id");
-  }
+    const roomName = req.url.replace("/", "");
+    if (!validator.isUUID(roomName)) {
+        ws.close();
+    }
 
-  ws.on(roomName, (data) => {
-    ws.send(data);
-  });
-
-  ws.on("message", (data, isBinary) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.emit(roomName, data);
-      }
+    ws.on(roomName, (data) => {
+        ws.send(data);
     });
-  });
+
+    ws.on("message", (data, isBinary) => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.emit(roomName, data);
+            }
+        });
+    });
 });
